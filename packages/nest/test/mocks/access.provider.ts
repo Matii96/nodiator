6371124073467b320f885c7291dcd549ca09556a
Mateusz Fonfara <mateusz.fonfara96@gmail.com>
2022-08-05 18:10:30 +0200

    typo fix

--- packages/nest/test/mocks/access.provider.ts
+++ packages/nest/test/mocks/access.provider.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { Mediator } from '@nodiator/core';
+import { IMediator } from '@nodiator/core';
 import { InjectMediator } from '../../lib';
 import { Namespaces } from './namespaces.enum';
 
 @Injectable()
 export class AccessProvider {
   constructor(
-    @InjectMediator() readonly globalMediator: Mediator,
-    @InjectMediator(Namespaces.NAMESPACE1) readonly namespace1Mediator: Mediator,
-    @InjectMediator(Namespaces.NAMESPACE2) readonly namespace2Mediator: Mediator
+    @InjectMediator() readonly globalMediator: IMediator,
+    @InjectMediator(Namespaces.NAMESPACE1) readonly namespace1Mediator: IMediator,
+    @InjectMediator(Namespaces.NAMESPACE2) readonly namespace2Mediator: IMediator
   ) {}
 }
