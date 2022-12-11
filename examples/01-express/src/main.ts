5beedb6317e1dffacea15d4c32a3bd9a9b3c8763
Matii96 <mateusz.fonfara96@gmail.com>
2022-12-11 13:02:28 +0100

    feat(nodiator-example-express): added logger to mediator instance

--- examples/01-express/src/main.ts
+++ examples/01-express/src/main.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { MediatorFactory } from '@nodiator/core';
+import { LoggerExtension, MediatorLoggingLevels } from '@nodiator/logger';
 import { itemsBootstrapper } from './items/items.bootstrapper';
 import { historyBootstrapper } from './history/history.bootstrapper';
 
 const app = express();
 const mediator = MediatorFactory.create({ dynamicOptions: () => ({ requests: { timeout: 10 } }) });
+mediator.use(new LoggerExtension({ dynamicOptions: () => ({ level: MediatorLoggingLevels.DEBUG }) }));
 
 itemsBootstrapper(app, mediator);
 historyBootstrapper(app, mediator);
