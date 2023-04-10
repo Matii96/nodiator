31b168085665ac87b629ef0b1017dd4e911aa6da
Matii96 <mateusz.fonfara96@gmail.com>
2023-04-10 20:39:56 +0200

    feat(core): changed event handlers return type to any
    Returned value is not used anyway therefore it's sometimes more handy to return other type than
    void.

--- packages/core/lib/messages/event/event-handler/event-handler.interface.ts
+++ packages/core/lib/messages/event/event-handler/event-handler.interface.ts
@@ -3,5 +3,5 @@ import { MessageProvider } from '../../interfaces/message-provider.interface';
 import { Event } from '../event';
 
 export interface IEventHandler<TEvent extends Event> extends MessageProvider {
-  handle(event: TEvent): Promise<void> | Observable<void>;
+  handle(event: TEvent): Promise<any> | Observable<any>;
 }
