17582eca7a4a75889afda9fcbd38a37fa8d39c63
Matii96 <mateusz.fonfara96@gmail.com>
2023-04-10 20:41:18 +0200

    fix(nest): inherit InjectMediator return type from Nest Inject

--- packages/nest/lib/injection/inject-mediator.decorator.ts
+++ packages/nest/lib/injection/inject-mediator.decorator.ts
@@ -6,5 +6,5 @@ import { getMediatorToken } from './get-mediator-token.factory';
  * Injects mediator instance.
  * @param {string} namespace Custom instance identifier. Allows to import mediator from outside of current module.
  */
-export const InjectMediator = (namespace?: string | symbol | Type): ParameterDecorator =>
-  namespace ? Inject(getMediatorToken(namespace)) : Inject(MEDIATOR_MODULE_FEATURE_INSTANCE);
+export const InjectMediator = (namespace?: string | symbol | Type) =>
+  Inject(namespace ? getMediatorToken(namespace) : MEDIATOR_MODULE_FEATURE_INSTANCE);
